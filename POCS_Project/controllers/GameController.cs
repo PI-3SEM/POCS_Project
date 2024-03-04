9ae78a3200c04b0fb566342b91423b45836dc5da
ChristianDev123 <christian.lima999@gmail.com>
2024-03-04 16:25:57 -0300

    game's list in development

---
+++ POCS_Project/controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using MagicTrickServer;
+using POCS_Project.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCS_Project.controllers
+{
+    public class GameController
+    {
+
+        public List<Game> Index()
+        {
+            var response = new List<Game>();
+                Jogo.ListarPartidas("T");
+            return response;
+        }
+    }
+}
---
+++ POCS_Project/entities/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCS_Project.entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public GameSituation Situation { get; set; }
+        public List<Player> Players {get;set;}
+    }
+    public enum GameSituation
+    {
+        // Open = A
+        Open = 0,
+        // Playing = T
+        Playing = 1,
+        // Closed = E
+        Closed = 2
+    }
+}
